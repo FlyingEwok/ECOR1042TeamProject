7486f7a7a2b5b211027d032b748fc3ce5b90e367
FlyingEwok <nicholas.garth1@gmail.com>
2021-11-30 16:28:18 -0500

    Changed if statements to match case & added python version check

--- Lab 4/T55_P4_case4.py
+++ Lab 4/T55_P4_case4.py
@@ -3,45 +3,65 @@
 
 from T55_P3_sorting import sort_books_title, sort_books_ascending_rate, sort_books_descending_rate, sort_books_publisher, sort_books_category, sort_books_pageCount
 from T55_P1_load_data import load_dataset
+import time
+import sys
 
+# Helper Functions
 def __invalidCommand() -> None:
-    """Prints Invalid Command and passes (error handling)"""
+    """Written by Nicholas Garth 101227727
+    Prints Invalid Command and passes (error handling)"""
     print("Invalid Command")
+    time.sleep(1) # Make it sleep so user can read it before it gets pushed up
     pass
 
+def __checkPythonVersion() -> None:
+    """Written by Nicholas Garth 101227727
+    Check the version of Python the user is running and terminate if they aren't"""
+    if sys.version_info < (3, 10):
+        print('Please upgrade your Python version to 3.10.0 or higher')
+        sys.exit()
+
+# Sort Book Call Function
 def sortBookCall(bookDict: dict) -> None:
-    """Asks user what sorting command they want to run on their dictionary
+    """Written by Nicholas Garth 101227727
+    Asks user what sorting command they want to run on their dictionary
     >>> sortBookCall(bookDict)
     T)itle   R)ate   P)ublisher  C)ategory   PA)ageCount    RE)turn
     Enter the upper-case letter to the left of the bracket of the option you want:
     """
+    # Check Python Version
+    __checkPythonVersion()
+
+    # Keep running the program until user returns to previous menu
     while True:
         print("\nT)itle   R)ate   P)ublisher  C)ategory   PA)ageCount    RE)turn")
         userInput = input("Enter the upper-case letter to the left of the bracket of the option you want: ")
-        if userInput == 'T':
-            sort_books_title(bookDict)
-        elif userInput == 'R':
-            while True: # Ask the user if they want ascending or descending order
-                print("\nA)scending   D)escending    RE)turn")
-                userInput = input("Enter the upper-case letter to the left of the bracket of the option you want: ")
-                if userInput == 'A':
-                    sort_books_ascending_rate(bookDict)
-                elif userInput == 'D':
-                    sort_books_descending_rate(bookDict)
-                elif userInput == 'RE':
-                    break
-                else:
-                    __invalidCommand()
-        elif userInput == 'P':
-            sort_books_publisher(bookDict)
-        elif userInput == 'C':
-            sort_books_category(bookDict)
-        elif userInput == 'PA':
-            sort_books_pageCount(bookDict)
-        elif userInput == 'RE':
-            break
-        else:
-            __invalidCommand()
+        match userInput:
+            case 'T':
+                sort_books_title(bookDict)
+            case 'R':
+                while True: # Ask the user if they want ascending or descending order
+                    print("\nA)scending   D)escending    RE)turn")
+                    userInput = input("Enter the upper-case letter to the left of the bracket of the option you want: ")
+                    match userInput:
+                        case 'A':
+                            sort_books_ascending_rate(bookDict)
+                        case 'D':
+                            sort_books_descending_rate(bookDict)
+                        case 'RE':
+                            break
+                        case _:
+                            __invalidCommand()
+            case 'P':
+                sort_books_publisher(bookDict)
+            case 'C':
+                sort_books_category(bookDict)
+            case 'PA':
+                sort_books_pageCount(bookDict)
+            case 'RE':
+                break
+            case _:
+                __invalidCommand()
         
 book_dict = load_dataset('Google_Books_Dataset.csv')
 sortBookCall(book_dict)
