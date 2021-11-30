a448cd21ea652e2836416c5d40b86389a73c2161
FlyingEwok <nicholas.garth1@gmail.com>
2021-11-30 16:27:27 -0500

    Changed if statements to match case, & added check python version

--- Lab 4/Team Code/T55_P2_booksUI.py
+++ Lab 4/Team Code/T55_P2_booksUI.py
@@ -8,12 +8,14 @@ from T55_P1_load_data import load_dataset
 from T55_P3_sorting import sort_books_title, sort_books_ascending_rate, sort_books_descending_rate, sort_books_publisher, sort_books_category, sort_books_pageCount
 from typing import Callable
 import time
+import sys
 
 # Helper Functions
 def __invalidCommand() -> None:
     """Written by Nicholas Garth 101227727
     Prints Invalid Command and passes (error handling)"""
     print("Invalid Command")
+    time.sleep(1) # Make it sleep so user can read it before it gets pushed up
     pass
 
 def __bookLoadCheck(command: Callable) -> None:
@@ -26,7 +28,14 @@ def __bookLoadCheck(command: Callable) -> None:
         command(bookDict)
     else:
         print("Please load the file first!")
-        time.sleep(1)
+        time.sleep(1) # Make it sleep so user can read it before it gets pushed up
+
+def __checkPythonVersion() -> None:
+    """Written by Nicholas Garth 101227727
+    Check the version of Python the user is running and terminate if they aren't"""
+    if sys.version_info < (3, 10):
+        print('Please upgrade your Python version to 3.10.0 or higher')
+        sys.exit()
 
 # User Input Functions
 def runProgram() -> None:
@@ -47,33 +56,38 @@ def runProgram() -> None:
 
     Enter the upper-case letter to the left of the bracket of the option you want:
     """
+    # Check what Python version the user is running and if running an old version terminate and tell them to upgrade
+    __checkPythonVersion() 
+       
+    # Keep running the program until user quits
     while True:
         print("\n1- Command Line L)oad file\n2- Command Line A)dd book\n3- Command Line R)emove book\n4- Command Line F)ind book by title\n5- Command Line NC) Number of books in a category\n6- Command Line CA) Categories for an author\n7- Command Line CB) Categories for a book title\n8- Command Line G)et book\n9- Command Line S)ort book\n10-Command line Q)uit")
         userInput = input("\nEnter the upper-case letter to the left of the bracket of the option you want: ")
-        if userInput == 'L':
-            global bookDict # make bookDict global so it can be used in any function
-            bookDict = load_dataset('Google_Books_Dataset.csv') # REPLACE WHAT THIS VAR = WITH THE LOADFILE FUNCTION!!!!!
-        elif userInput == 'A':
-            pass # Replace pass with function associated
-        elif userInput == 'R':
-            pass # Replace pass with function associated
-        elif userInput == 'F':
-            pass # Replace pass with function associated
-        elif userInput == 'NC':
-            pass # Replace pass with function associated
-        elif userInput == 'CA':
-            pass # Replace pass with function associated
-        elif userInput == 'CB':
-            pass # Replace pass with function associated
-        elif userInput == 'G':
-            pass # Replace pass with function associated
-        elif userInput == 'S':
-            __bookLoadCheck(sortBookCall)
-        elif userInput == 'Q':
-            print("Thank You for using our program\nBye Bye!")
-            break
-        else:
-            __invalidCommand()
+        match userInput:
+            case 'L':
+                global bookDict # make bookDict global so it can be used in any function
+                bookDict = load_dataset('Google_Books_Dataset.csv') # REPLACE WHAT THIS VAR = WITH THE LOADFILE FUNCTION!!!!!
+            case 'A':
+                pass # Replace pass with function associated
+            case 'R':
+                pass # Replace pass with function associated
+            case 'F':
+                pass # Replace pass with function associated
+            case 'NC':
+                pass # Replace pass with function associated
+            case 'CA':
+                pass # Replace pass with function associated
+            case 'CB':
+                pass # Replace pass with function associated
+            case 'G':
+                pass # Replace pass with function associated
+            case 'S':
+                __bookLoadCheck(sortBookCall)
+            case 'Q':
+                print("Thank You for using our program\nBye Bye!")
+                break
+            case _:
+                __invalidCommand()
 
 # Function 9
 def sortBookCall(bookDict: dict) -> None:
@@ -83,33 +97,36 @@ def sortBookCall(bookDict: dict) -> None:
     T)itle   R)ate   P)ublisher  C)ategory   PA)ageCount    RE)turn
     Enter the upper-case letter to the left of the bracket of the option you want:
     """
+    # Keep running the program until user returns to previous menu
     while True:
         print("\nT)itle   R)ate   P)ublisher  C)ategory   PA)ageCount    RE)turn")
         userInput = input("Enter the upper-case letter to the left of the bracket of the option you want: ")
-        if userInput == 'T':
-            sort_books_title(bookDict)
-        elif userInput == 'R':
-            while True: # Ask the user if they want ascending or descending order
-                print("\nA)scending   D)escending    RE)turn")
-                userInput = input("Enter the upper-case letter to the left of the bracket of the option you want: ")
-                if userInput == 'A':
-                    sort_books_ascending_rate(bookDict)
-                elif userInput == 'D':
-                    sort_books_descending_rate(bookDict)
-                elif userInput == 'RE':
-                    break
-                else:
-                    __invalidCommand()
-        elif userInput == 'P':
-            sort_books_publisher(bookDict)
-        elif userInput == 'C':
-            sort_books_category(bookDict)
-        elif userInput == 'PA':
-            sort_books_pageCount(bookDict)
-        elif userInput == 'RE':
-            break
-        else:
-            __invalidCommand()
+        match userInput:
+            case 'T':
+                sort_books_title(bookDict)
+            case 'R':
+                while True: # Ask the user if they want ascending or descending order
+                    print("\nA)scending   D)escending    RE)turn")
+                    userInput = input("Enter the upper-case letter to the left of the bracket of the option you want: ")
+                    match userInput:
+                        case 'A':
+                            sort_books_ascending_rate(bookDict)
+                        case 'D':
+                            sort_books_descending_rate(bookDict)
+                        case 'RE':
+                            break
+                        case _:
+                            __invalidCommand()
+            case 'P':
+                sort_books_publisher(bookDict)
+            case 'C':
+                sort_books_category(bookDict)
+            case 'PA':
+                sort_books_pageCount(bookDict)
+            case 'RE':
+                break
+            case _:
+                __invalidCommand()
 
 # Run the program
 runProgram()
