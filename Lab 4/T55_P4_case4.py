68246d3f51af6c93d042d957e0fe2307c3d9024d
FlyingEwok <nicholas.garth1@gmail.com>
2021-11-30 13:44:10 -0500

    Created Sort Book Function (Untested)

--- Lab 4/T55_P4_case4.py
+++ Lab 4/T55_P4_case4.py
@@ -1,3 +1,38 @@
 # Team T-55
 # Nicholas Garth 101227727
 
+from T55_P3_sorting import sort_books_title, sort_books_ascending_rate, sort_books_descending_rate, sort_books_publisher, sort_books_category, sort_books_pageCount
+
+def __invalidCommand() -> None:
+    print("Invalid Command")
+    pass
+
+def sortBookCall(bookDict: dict) -> None:
+    while True:
+        print("T)itle   R)ate   P)ublisher  C)ategory   PA)ageCount    RE)turn")
+        userInput = input("Enter the upper-case letter to the left of the bracket of the option you want: ")
+        if userInput == 'T':
+            sort_books_title(bookDict)
+        elif userInput == 'R':
+            while True: # Ask the user if they want ascending or descending order
+                print("A)scending   D)escending    RE)turn")
+                userInput = input("Enter the upper-case letter to the left of the bracket of the option you want: ")
+                if userInput == 'A':
+                    sort_books_ascending_rate(bookDict)
+                elif userInput == 'D':
+                    sort_books_descending_rate(bookDict)
+                elif userInput == 'RE':
+                    break
+                else:
+                    __invalidCommand()
+        elif userInput == 'P':
+            sort_books_publisher(bookDict)
+        elif userInput == 'C':
+            sort_books_category(bookDict)
+        elif userInput == 'PA':
+            sort_books_pageCount(bookDict)
+        elif userInput == 'RE':
+            break
+        else:
+            __invalidCommand()
+        
