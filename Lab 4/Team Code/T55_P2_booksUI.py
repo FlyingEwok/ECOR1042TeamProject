ad4ae5b9892e6bc5add60b748c3f0541180d6ea1
FlyingEwok <nicholas.garth1@gmail.com>
2021-12-03 14:13:23 -0500

    Imported Case3

--- Lab 4/Team Code/T55_P2_booksUI.py
+++ Lab 4/Team Code/T55_P2_booksUI.py
@@ -9,6 +9,7 @@
 # Imports
 from T55_P1_load_data import load_dataset
 from T55_P3_sorting import sort_books_title, sort_books_ascending_rate, sort_books_descending_rate, sort_books_publisher, sort_books_category, sort_books_pageCount
+from T55_P2_search_modify_dataset import get_books_by_rate, get_books_by_author, get_books_by_publisher,  get_books_by_category,all_categories_for_book_title, get_book_by_category_and_rate
 from typing import Callable
 import time
 import sys
@@ -83,13 +84,50 @@ def runProgram() -> None:
             case 'CB':
                 pass # Replace pass with function associated
             case 'G':
-                pass # Replace pass with function associated
+                __bookLoadCheck(get_book_call)
             case 'S':
                 __bookLoadCheck(sortBookCall)
             case 'Q':
                 print("Thank You for using our program\nBye Bye!")
                 break
             case _:
+                __invalidCommand()
+
+# Function 8
+def get_book_call(book_dict: dict) -> None:
+    """
+    Function designed by Spencer Green
+    Determines the way in which books of a data set will be retrieved when given a command including( R)ate, A)uthor, P)ublisher, C)ategory, CT)Category and Title, and CR)Category and Rate.
+    >>>get_book_call(book_dict ).
+    R)ate A)uthor P)ublisher C)ategory CT)Category and Title CR)Category and Rate.
+    Enter an upper-case letter seen above to determine the way books are to be retrieved: 
+    """
+    while True:
+        print("\nR)ate A)uthor P)ublisher C)ategory CT)Category and Title CR)Category and Rate RE)turn.")
+        user_input = input("Enter an upper-case letter seen above to determine the way books are to be retrieved: " )
+        match user_input:
+            case 'R':
+                rate_input = input("Enter the desired book rating: ")
+                get_books_by_rate(float(rate_input), book_dict)
+            case 'A':
+                author_input = input("Enter the desired book author: ")
+                get_books_by_author(book_dict, author_input)
+            case 'P':
+                publisher_input = input("Enter the desired book publisher: ")
+                get_books_by_publisher(publisher_input, book_dict)
+            case 'C':
+                category_input = input("Enter the desired book category: ")
+                get_books_by_category(book_dict, category_input)
+            case 'CT':
+                title_input = input("Enter the desired book title: ")
+                all_categories_for_book_title(title_input, book_dict)
+            case 'CR':
+                category_input = input("Enter the desired book category: ")
+                rate_input = input("Enter the desired book rating: ")
+                get_book_by_category_and_rate(category_input, rate_input, book_dict)
+            case 'RE':
+                break 
+            case _: 
                 __invalidCommand()
 
 # Function 9
